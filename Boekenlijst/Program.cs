53a43a2f32eb13e6b6cac7bc16c4c11455781797
Jelle.db050503 <jelle.db050503@gmailcom>
2026-03-08 17:31:56 +0100

    fixed isue

--- Boekenlijst/Program.cs
+++ Boekenlijst/Program.cs
@@ -1,5 +1,6 @@
 using Boekenlijst.Models;
 using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,12 +30,11 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = ResolveConnectionString(builder.Configuration);
+
 // Add DbContext for MySQL
 builder.Services.AddDbContext<BoekenLijstContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
-    )
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
 );
 
 var app = builder.Build();
@@ -55,3 +55,45 @@ app.UseStaticFiles();
 app.MapControllers();
 
 app.Run();
+
+static string ResolveConnectionString(IConfiguration configuration)
+{
+    var direct = configuration.GetConnectionString("DefaultConnection");
+    if (!string.IsNullOrWhiteSpace(direct))
+    {
+        return direct;
+    }
+
+    var mysqlUrl = Environment.GetEnvironmentVariable("MYSQL_URL")
+        ?? Environment.GetEnvironmentVariable("DATABASE_URL")
+        ?? Environment.GetEnvironmentVariable("MYSQLPUBLIC_URL");
+
+    if (!string.IsNullOrWhiteSpace(mysqlUrl))
+    {
+        return ConvertMySqlUrlToConnectionString(mysqlUrl);
+    }
+
+    throw new InvalidOperationException(
+        "No database connection string found. Configure ConnectionStrings__DefaultConnection or MYSQL_URL in Railway environment variables.");
+}
+
+static string ConvertMySqlUrlToConnectionString(string mysqlUrl)
+{
+    var uri = new Uri(mysqlUrl);
+    var userInfo = uri.UserInfo.Split(':', 2);
+    var user = Uri.UnescapeDataString(userInfo[0]);
+    var password = userInfo.Length > 1 ? Uri.UnescapeDataString(userInfo[1]) : string.Empty;
+    var database = uri.AbsolutePath.TrimStart('/');
+
+    var builder = new MySqlConnectionStringBuilder
+    {
+        Server = uri.Host,
+        Port = (uint)(uri.Port > 0 ? uri.Port : 3306),
+        UserID = user,
+        Password = password,
+        Database = database,
+        SslMode = MySqlSslMode.Preferred
+    };
+
+    return builder.ConnectionString;
+}
